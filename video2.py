61e97163c822cbb70d57d6416374a7c1a1e37318
slaven <slaven92@gmail.com>
2019-05-30 16:28:33 +0200

    begging of video 5

--- video2.py
+++ video2.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import quandl
 import math
+import numpy as np
+from sklearn import preprocessing, svm
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -18,12 +22,20 @@ forecast_out = int(math.ceil(0.01*len(df)))
 df['label'] = df[forecast_col].shift(-forecast_out)
 df.dropna(inplace=True)
 
+X = np.array(df.drop(['label'], 1))
+y = np.array(df['label'])
+X = preprocessing.scale(X)
+y = np.array(df['label'])
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-
-
-
-
+clf = LinearRegression(n_jobs=-1)
+# clf = svm.SVR(kernel='linear')
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test,y_test)
+accuracy
 # %%
+len(X)
+len(y)
 df.head()
 df.tail()
