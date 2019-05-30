6a88de4d9ab36dc7646acfec840851b6b460cb4c
slaven <slaven92@gmail.com>
2019-05-30 17:05:19 +0200

    video 6

--- video2.py
+++ video2.py
@@ -1,13 +1,18 @@
 import pandas as pd
-import quandl
-import math
+import quandl, math, datetime
+import matplotlib.pyplot as plt
+from matplotlib import style
 import numpy as np
 from sklearn import preprocessing, svm
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
-df = quandl.get('WIKI/GOOGL')
+style.use('ggplot')
 
+df = quandl.get('WIKI/GOOGL')
+cp = df.copy()
+# %%
+df = cp.copy()
 df = df[["Adj. Open","Adj. High","Adj. Low","Adj. Close","Adj. Volume"]]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close'])/df['Adj. Close'] * 100
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open'])/df['Adj. Open'] * 100
@@ -20,20 +25,41 @@ forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 forecast_out = int(math.ceil(0.01*len(df)))
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
 
 X = np.array(df.drop(['label'], 1))
-y = np.array(df['label'])
 X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+df.dropna(inplace=True)
 y = np.array(df['label'])
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
+# %%
 clf = LinearRegression(n_jobs=-1)
 # clf = svm.SVR(kernel='linear')
 clf.fit(X_train, y_train)
 accuracy = clf.score(X_test,y_test)
-accuracy
+
+forcast_set = clf.predict(X_lately)
+print(forcast_set, accuracy, forecast_out)
+
+# %%
+df['Forcast'] = np.NaN
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forcast_set:
+    next_day = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(df.colums-1)] + [i]
+df['Adj. Close'].plot()
+df['Forcast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
+
 # %%
 len(X)
 len(y)
