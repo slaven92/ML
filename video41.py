5a25b7974c344c3ba09eca42838ebc6df5423708
slaven <slaven92@gmail.com>
2019-06-04 18:29:33 +0200

    video 41 shift cluster from scratch

---
+++ video41.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
+
+# %%
+X = np.array([[1, 2], [1.5, 1.8], [5, 8], [8, 8], [1, 0.6], [9, 11]])
+
+plt.scatter(X[:,0],X[:,1],s=150, linewidths=5)
+plt.show()
+# %%
+
+colors = 10 * ["g", "r", "c", "b", "k", "y "]
