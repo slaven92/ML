b6439f7099032db22ca6b5780b5fbb70464c4e3e
slaven <slaven92@gmail.com>
2019-06-04 16:44:52 +0200

    k means starting

---
+++ video37.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
+import numpy as np
+from sklearn.cluster import KMeans
+
+# %%
+X = np.array([[1, 2],[1.5, 1.8],[5,8],[8,8],[1,0.6],[9,11]])
+
+# plt.scatter(X[:,0],X[:,1],s=150, linewidths=5)
+# plt.show()
+# %%
+clf = KMeans(n_clusters=6)
+clf.fit(X)
+
+centroid = clf.cluster_centers_
+labels = clf.labels_
+
+colors = 10*["g.","r.","c.","b.","k.","y."]
